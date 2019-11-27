44337e669d734a72f598ba8bc2b3fec8bda61ba1
Ivan Ray Altomera <irayspacii@gmail.com>
2019-11-27 17:12:41 +0800

    feat: InPatient Record customer filter

--- vet_care/public/js/inpatient_record.js
+++ vet_care/public/js/inpatient_record.js
@@ -1,4 +1,7 @@
 frappe.ui.form.on('Inpatient Record', {
+    refresh: async function(frm) {
+        await vet_care.utils.patient_customers_by_pet_relations(frm, 'patient', 'vc_customer');
+    },
     patient: async function(frm) {
         await vet_care.utils.patient_customers_by_pet_relations(frm, 'patient', 'vc_customer');
     }
