f6926fd341c80d70decd8a5f316ab4899db6f094
Ivan Ray Altomera <irayspacii@gmail.com>
2019-11-27 15:56:31 +0800

    feat(InPatient Record): added customer filter by patient

--- vet_care/public/js/inpatient_record.js
+++ vet_care/public/js/inpatient_record.js
@@ -1,10 +1,10 @@
-frappe.ui.form.on('Sales Invoice', {
+frappe.ui.form.on('Inpatient Record', {
     patient: async function(frm) {
         const { message: pet_relations } = await frappe.call({
             method: "vet_care.api.get_pet_relations",
             args: { pet: frm.doc.patient }
         });
-        frm.set_query('customer', function() {
+        frm.set_query('vc_customer', function() {
             return {
                 filters: { 'name': ['in', pet_relations] }
             };
