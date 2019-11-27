2e0a99c84faa4da0c9e863a811c1bfcd76fded48
Ivan Ray Altomera <irayspacii@gmail.com>
2019-11-27 17:05:02 +0800

    refactor: use vet_care.utils function

--- vet_care/public/js/inpatient_record.js
+++ vet_care/public/js/inpatient_record.js
@@ -1,13 +1,5 @@
 frappe.ui.form.on('Inpatient Record', {
     patient: async function(frm) {
-        const { message: pet_relations } = await frappe.call({
-            method: "vet_care.api.get_pet_relations",
-            args: { pet: frm.doc.patient }
-        });
-        frm.set_query('vc_customer', function() {
-            return {
-                filters: { 'name': ['in', pet_relations] }
-            };
-        });
+        await vet_care.utils.patient_customers_by_pet_relations(frm, 'patient', 'vc_customer');
     }
 });
--- vet_care/public/js/sales_invoice.js
+++ vet_care/public/js/sales_invoice.js
@@ -1,13 +1,5 @@
 frappe.ui.form.on('Sales Invoice', {
     patient: async function(frm) {
-        const { message: pet_relations } = await frappe.call({
-            method: "vet_care.api.get_pet_relations",
-            args: { pet: frm.doc.patient }
-        });
-        frm.set_query('customer', function() {
-            return {
-                filters: { 'name': ['in', pet_relations] }
-            };
-        });
+        await vet_care.utils.patient_customers_by_pet_relations(frm);
     }
 });
