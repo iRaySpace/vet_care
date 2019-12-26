69998a3aeabf4079316a341e6eccb0e1ae03f375
Ivan Ray Altomera <irayspacii@gmail.com>
2019-12-26 18:51:35 +0800

    refactor: vetcare settings to v11

--- vet_care/vet_care/doctype/vetcare_settings/vetcare_settings.js
+++ vet_care/vet_care/doctype/vetcare_settings/vetcare_settings.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Vetcare Settings', {
-	refresh: function(frm) {
-
+	apply_core_overrides: async function(frm) {
+		const { message: data } = await frappe.call({ method: 'vet_care.api.apply_core_overrides' });
+		frappe.msgprint(`Overrided core: ${JSON.stringify(data)}`);
 	}
 });
