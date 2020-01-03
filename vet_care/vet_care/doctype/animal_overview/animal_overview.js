70b4c44a2230a6dfd3f54201a8027ad909283405
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-03 18:52:51 +0800

    feat: animal overview show details

--- vet_care/vet_care/doctype/animal_overview/animal_overview.js
+++ vet_care/vet_care/doctype/animal_overview/animal_overview.js
@@ -3,6 +3,17 @@
 
 frappe.ui.form.on('Animal Overview', {
 	refresh: function(frm) {
-
+		frm.disable_save();
+	},
+	animal: async function(frm) {
+		const patient = await frappe.db.get_doc('Patient', frm.doc.animal);
+		frm.set_value('animal_name', patient.patient_name);
+		frm.set_value('species', patient.vc_species);
+		frm.set_value('color', patient.vc_color);
+		frm.set_value('sex', patient.sex);
+		frm.set_value('dob', patient.dob);
+		frm.set_value('weight', patient.vc_weight);
+		frm.set_value('default_owner', patient.customer);
+		frm.set_value('breed', patient.vc_breed);
 	}
 });
