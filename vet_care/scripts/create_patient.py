557f77f48363fbd5a83635baa8c45a2066c880ac
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-20 16:53:58 +0800

    fix: create patient

--- vet_care/scripts/create_patient.py
+++ vet_care/scripts/create_patient.py
@@ -12,6 +12,7 @@ def execute(filename):
             print(f'Importing {row.get("vc_cirrusvet")}...')
             patient = frappe.new_doc('Patient')
             patient.update({
+                'patient_name': row.get('patient_name'),
                 'sex': row.get('sex'),
                 'vc_weight': row.get('vc_weight'),
                 'vc_species': row.get('vc_species'),
