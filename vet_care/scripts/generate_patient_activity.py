32fea4a96af1d67f2b1cd6d9695f9ee3548c71bd
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-20 17:26:52 +0800

    fix: generate patient_activity

--- vet_care/scripts/generate_patient_activity.py
+++ vet_care/scripts/generate_patient_activity.py
@@ -1,15 +1,17 @@
 import frappe
 import csv
 from toolz import first
+import datetime
 
 
+# bench execute vet_care.scripts.generate_patient_activity.execute --args "['./data/', './data/processed_patient_activity_new.csv']"
 def execute(filename, processed_filename):
     patient_activities = []
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             cirrus_animal_id = row.get('cirrus_animal_id')
-            visit_date = row.get('visit_date')
+            visit_date = datetime.datetime.strptime(row.get('visit_date'), '%d-%m-%Y')
             patient_activity = _generate_patient_activity(cirrus_animal_id, visit_date)
             if patient_activity:
                 patient_activities.append({
@@ -43,3 +45,8 @@ def _generate_patient_activity(cirrusvet_id, posting_date):
 
 def _get_patients(cirrusvet_id):
     return frappe.get_all('Patient', filters={'vc_cirrusvet': cirrusvet_id})
+
+
+def test():
+    test_date = datetime.datetime.strptime('01-06-2017', '%d-%m-%Y')
+    print(test_date.date())
