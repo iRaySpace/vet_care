c2ab5563609804ea71aed98cd0561e09b3f41ea5
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-22 18:15:30 +0800

    feat: initial `Patient Booking Calendar`

--- vet_care/vet_care/doctype/patient_booking/patient_booking.py
+++ vet_care/vet_care/doctype/patient_booking/patient_booking.py
@@ -4,7 +4,61 @@
 
 from __future__ import unicode_literals
 import frappe
+import datetime
 from frappe.model.document import Document
+from toolz import compose, partial
+
 
 class PatientBooking(Document):
-	pass
+	def validate(self):
+		customer_name = frappe.get_value('Customer', self.customer, 'customer_name')
+		patient_name = frappe.get_value('Patient', self.patient, 'patient_name')
+		practitioner_name = frappe.get_value('Healthcare Practitioner', self.physician, 'last_name')
+		self.customer_name = customer_name
+		self.patient_name = patient_name
+		self.physician_name = practitioner_name
+
+
+@frappe.whitelist()
+def get_events(start, end, filters=None):
+	"""
+	Returns events for Gantt / Calendar view rendering.
+	:param start:
+	:param end:
+	:param filters:
+	:return: {'name', 'title', 'start', 'end'}
+	"""
+	def get_data(data):
+		print(data)
+		return {
+			'allDay': 0,
+			'name': data.get('name'),
+			'start': data.get('start'),
+			'end': data.get('start') + datetime.timedelta(minutes=30.0),
+			'title': '; '.join([
+				data.get('customer_name') or 'NA',
+				data.get('patient_name') or 'NA',
+				data.get('physician_name') or 'NA'
+			])
+		}
+
+	args = {
+		'start': start,
+		'end': end
+	}
+
+	return compose(
+		partial(map, get_data)
+	)(
+		frappe.db.sql("""
+			SELECT
+				pb.name,
+				pb.customer_name,
+				pb.patient_name,
+				pb.physician_name,
+				TIMESTAMP(pb.appointment_date, pb.appointment_time) as start
+			FROM `tabPatient Booking` pb
+			WHERE (pb.appointment_date BETWEEN %(start)s AND %(end)s)
+			AND pb.docstatus < 2
+		""", args, as_dict=True)
+	)
---
+++ vet_care/vet_care/doctype/patient_booking/patient_booking_calendar.js
@@ -0,0 +1,12 @@
+frappe.views.calendar['Patient Booking'] = {
+  gantt: true,
+  order_by: 'appointment_date',
+  get_events_method: 'vet_care.vet_care.doctype.patient_booking.patient_booking.get_events',
+  field_map: {
+    'start': 'start',
+    'end': 'end',
+    'id': 'name',
+    'title': 'title',
+    'allDay': 'allDay'
+  }
+};
