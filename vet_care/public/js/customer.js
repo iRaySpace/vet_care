b11260528062dccbf81bdf55cc1c92d41e764f4b
Ivan Ray Altomera <irayspacii@gmail.com>
2020-03-05 20:24:31 +0800

    feat: set dashboard

--- vet_care/public/js/customer.js
+++ vet_care/public/js/customer.js
@@ -1,15 +1,31 @@
 frappe.ui.form.on('Customer', {
     refresh: function(frm) {
-        _add_patient_link(frm);
+        _set_dashboard(frm);
     }
 });
 
-function _add_patient_link(frm) {
-    const pre_sales = frm.dashboard.data.transactions.find(({ label }) => label === __('Pre Sales'));
-    if (pre_sales && !pre_sales.items.includes('Patient')) {
-        pre_sales.items = ['Patient', ...pre_sales.items];
-        frm.dashboard.data_rendered = false;
-        frm.dashboard.transactions_area.empty();
-        frm.dashboard.refresh();
-    }
+function _set_dashboard(frm) {
+    // Remove Subscriptions
+    const filtered_sections = [__('Subscriptions')];
+    const filtered_transactions = frm.dashboard.data.transactions.filter(function(transaction) {
+        return !filtered_sections.includes(transaction.label);
+    });
+    frm.dashboard.data.transactions = filtered_transactions;
+
+    // Remove Sales Order, Delivery Note under Orders
+    const filtered_orders_items = ['Sales Order', 'Delivery Note'];
+    const orders = filtered_transactions.find(function(transaction) {
+        return transaction.label === __('Orders');
+    });
+    orders.items = orders.items.filter(function(item) {
+        return !filtered_orders_items.includes(item);
+    });
+
+    // Add Patient on Orders
+    orders.items.push('Patient');
+
+    // Reload dashboard
+    frm.dashboard.data_rendered = false;
+    frm.dashboard.transactions_area.empty();
+    frm.dashboard.refresh();
 }
