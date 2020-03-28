f9c3b0079eedb368c9d9b2f38076ed28339c95f2
Sun Howwrongbum <sun@libermatic.com>
2020-03-28 21:02:34 +0000

    feat(Customer): add search_values

--- vet_care/hooks.py
+++ vet_care/hooks.py
@@ -54,6 +54,7 @@ fixtures = [
                     "Customer-mobile_number",
                     "Customer-mobile_number_2",
                     "Customer-vc_area",
+                    "Customer-vc_search_values",
                     "Healthcare Practitioner-vc_color",
                     "Patient-vc_inpatient",
                     "Healthcare Practitioner-vc_out_of_clinic"
