df80c68597442678b458c0b3eba3dca732017379
Ivan Ray Altomera <irayspacii@gmail.com>
2020-08-07 19:39:34 +0800

    feat(animal_overview): attachment

--- vet_care/vet_care/doctype/animal_overview/animal_overview.py
+++ vet_care/vet_care/doctype/animal_overview/animal_overview.py
@@ -4,7 +4,45 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+from toolz import compose, first, pluck
 from frappe.model.document import Document
+from functools import partial
+
 
 class AnimalOverview(Document):
-	pass
+	def validate(self):
+		_set_attach_to_animal(self)
+
+
+def _set_attach_to_animal(animal_overview):
+	if not animal_overview.animal and animal_overview.attach:
+		frappe.throw(_('Please set animal field'))
+	file = compose(
+		first,
+		partial(pluck, 'name'),
+	)(
+		frappe.get_all(
+			'File',
+			filters={
+				'attached_to_name': 'Animal Overview',
+				'attached_to_doctype': 'Animal Overview'
+			}
+		)
+	)
+	frappe.db.sql(
+		"""
+			UPDATE 
+				`tabFile`
+			SET
+				attached_to_name = %(attached_to_name)s,
+				attached_to_doctype = 'Patient'
+			WHERE
+				name = %(name)s
+		""",
+		{
+			'attached_to_name': animal_overview.animal,
+			'name': file
+		}
+	)
+	animal_overview.attach = None
