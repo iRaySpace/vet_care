5a20dded95bbefb37419347692f534ff308c1773
Ivan Ray Altomera <irayspacii@gmail.com>
2020-08-13 16:31:02 +0800

    feat(patient_booking_buttons): set sales_person on Patient Booking

--- vet_care/vet_care/doctype/patient_booking/patient_booking_buttons.js
+++ vet_care/vet_care/doctype/patient_booking/patient_booking_buttons.js
@@ -3,12 +3,20 @@ function set_custom_buttons(frm) {
         {
             label: __('Animal Overview'),
             onclick: async function() {
+                const sales_person = await _get_practitioner(frm.doc.physician);
                 await frappe.set_route('Form', 'Animal Overview');
                 frappe.model.set_value('Animal Overview', 'Animal Overview', 'animal', frm.doc.patient);
+                frappe.model.set_value('Animal Overview', 'Animal Overview', 'sales_person', sales_person.employee);
             },
         }
     ];
     custom_buttons.forEach((custom_button) => {
         frm.add_custom_button(custom_button['label'], custom_button['onclick']);
     });
+}
+
+
+async function _get_practitioner(physician) {
+    const { message: employee } = await frappe.db.get_value('Healthcare Practitioner', physician, 'employee');
+    return employee;
 }
