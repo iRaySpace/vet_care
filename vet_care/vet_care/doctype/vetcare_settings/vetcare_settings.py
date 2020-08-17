85ac560fbddaf7ab4190ff922217bae2888239f6
Ivan Ray Altomera <irayspacii@gmail.com>
2020-08-17 17:50:10 +0800

    feat(vetcare_settings): throw pos bahrain

--- vet_care/vet_care/doctype/vetcare_settings/vetcare_settings.py
+++ vet_care/vet_care/doctype/vetcare_settings/vetcare_settings.py
@@ -4,7 +4,19 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
+
 class VetcareSettings(Document):
-	pass
+	def validate(self):
+		_check_pos_bahrain()
+
+
+def _check_pos_bahrain():
+	pos_bahrain = frappe.get_all(
+		'Module Def',
+		filters={'name': 'Pos Bahrain'}
+	)
+	if not pos_bahrain:
+		frappe.throw(_('Please install POS Bahrain app'))
